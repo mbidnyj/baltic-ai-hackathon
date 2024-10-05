e6b42f0f7db778048956841023655f9a84b4e5fd
Doston Nabotov <dostonnabotov@gmail.com>
2024-10-05 23:26:15 +0300

    Fixed header

--- baltic-ai-hack-frontend/src/App.js
+++ baltic-ai-hack-frontend/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TeacherDashboard from './components/TeacherDashboard';
-import ModulePreview from './components/ModulePreview';
-import EditModule from './components/EditModule'; // Import the new EditModule component
-import Layout from './components/Layout';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import TeacherDashboard from "./components/TeacherDashboard";
+import ModulePreview from "./components/ModulePreview";
+import EditModule from "./components/EditModule"; // Import the new EditModule component
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<TeacherDashboard />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<TeacherDashboard />} />
           <Route path="/module/:moduleId/preview" element={<ModulePreview />} />
-          <Route path="/module/:moduleId/edit" element={<EditModule />} /> {/* New Edit route */}
-        </Routes>
+          <Route path="/module/:moduleId/edit" element={<EditModule />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
